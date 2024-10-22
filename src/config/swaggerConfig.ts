c6ede7d263fdf31f0e57f19e34a1dd6d1383d686
Harshit Chandasana <harshit@tatvic.com>
2024-10-22 14:42:46 +0530

    Updated swagger options

--- src/config/swaggerConfig.ts
+++ src/config/swaggerConfig.ts
@@ -4,16 +4,11 @@ const swaggerOptions: Options = {
     definition: {
         openapi: '3.0.0',
         info: {
-            title: 'Express API with Swagger',
+            title: 'API Docs',
             version: '1.0.0',
             description:
-                'A simple CRUD API application made with Express and documented with Swagger',
-        },
-        servers: [
-            {
-                url: 'http://localhost:8080',
-            },
-        ],
+                'An API application made with Express and documented with Swagger',
+        }
     },
     apis: ['./src/routes/*.ts', './src/models/*.ts'], // Files containing annotations
 };
